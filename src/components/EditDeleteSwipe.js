2ee76d4661347c57c6f022c06204fcf5aee66764
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-03-26 15:20:55 +0800

    Optomize EditDeleteSwipe and ListItemContainer

--- src/components/EditDeleteSwipe.js
+++ src/components/EditDeleteSwipe.js
@@ -1,14 +1,38 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React, { Component } from 'react'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import FA from '@expo/vector-icons/FontAwesome6'
+import theme from '../styles/theme'
 
-export default class EditDeleteSwipe extends Component {
-  render() {
-    return (
-      <View>
-        <Text>EditDeleteSwipe</Text>
-      </View>
-    )
-  }
+const EditDeleteSwipe = () => {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <TouchableOpacity onPress={() => console.log('delete function')}>
+        <View style={[styles.container, styles.deleteColor]}>
+          <FA name='trash' size={20} />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => console.log('edit function')}>
+        <View style={[styles.container, styles.editColor]}>
+          <FA name='pen-to-square' size={20} />
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({})
+export default EditDeleteSwipe
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 80,
+    width: 100,
+  },
+  deleteColor: {
+    backgroundColor: theme.background.deleteColor,
+  },
+  editColor: {
+    backgroundColor: theme.background.editColor,
+  },
+})
--- src/components/ListItemContainer.js
+++ src/components/ListItemContainer.js
@@ -1,25 +1,12 @@
 import React from 'react'
 import ListItemDisplay from './ListItemDisplay'
-import { StyleSheet, TouchableOpacity, View } from 'react-native'
 import { GestureHandlerRootView, Swipeable } from 'react-native-gesture-handler'
-import FA from '@expo/vector-icons/FontAwesome6'
-import theme from '../styles/theme'
+import EditDeleteSwipe from './EditDeleteSwipe'
 
 const ListItemContainer = ({ item }) => {
 
   const renderRightActions = () => (
-    <View style={{flexDirection: 'row'}}>
-      <TouchableOpacity onPress={() => console.log('delete function')}>
-        <View style={[styles.container, styles.deleteColor]}>
-          <FA name='trash' size={20} />
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => console.log('edit function')}>
-        <View style={[styles.container, styles.editColor]}>
-          <FA name='pen-to-square' size={20} />
-        </View>
-      </TouchableOpacity>
-    </View>
+    <EditDeleteSwipe />
   )
 
   return (
@@ -31,19 +18,6 @@ const ListItemContainer = ({ item }) => {
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 80,
-    width: 100,
-  },
-  deleteColor: {
-    backgroundColor: theme.background.deleteColor,
-  },
-  editColor: {
-    backgroundColor: theme.background.editColor,
-  },
-})
+
 
 export default ListItemContainer
